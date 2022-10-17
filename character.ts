15761cc4bd645aa113c69a25682a8abbf80f39e3
Liluminalia <peloazul4@gmail.com>
2022-10-17 17:55:26 +0200

    creating documents ts

---
+++ character.ts
@@ -0,0 +1,21 @@
+export class Character {
+    name: string;
+    family: string;
+    age: number;
+    lifeStatus: boolean;
+    message: string;
+    static serie = 'Game of Thrones';
+    constructor(name: string, family: string, age: number) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+        this.lifeStatus = true;
+        this.message = '';
+    }
+    communicate() {
+        return this.message;
+    }
+    die() {
+        this.lifeStatus = false;
+    }
+}
